2ddb9eb157c6b08c2068fc6d0ef819d76810e597
Chaotic-Potato <andy825825@gmail.com>
2016-03-06 19:25:55 -0600

    Added Gravity

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 var Universe =  {
 	bodyArray: [],
+	tickRate: 100,
+	timeScale: 1,
 	addBody: function(name, mass, x, y, velX, velY) {
 		Universe.bodyArray.push(new Universe.body(name, mass, x, y, velX, velY))
 	 },
 	 tick: function () {
 		 Render.clear()
-		 for (i in Universe.bodyArray) {
-			Render.renderObject("object", Universe.bodyArray[i].x, Universe.bodyArray[i].y, 50, 50)
-			//Universe.bodyArray[i].tick()
+		 if (Universe.bodyArray.length > 250) {
+			Universe.bodyArray.splice(0, 1)
 		 }
+		 for (i in Universe.bodyArray) {
+			Universe.bodyArray[i].preTick()
+		 }
+		 for (i in Universe.bodyArray) {
+			Render.renderObject("object", Universe.bodyArray[i].x, Universe.bodyArray[i].y, Math.pow((Universe.bodyArray[i].mass), (1/3)), Math.pow((Universe.bodyArray[i].mass), (1/3)))
+			Universe.bodyArray[i].postTick()
+		 }
+	 },
+	 buttonAdd: function() {
+		 Universe.addBody(document.getElementById("name").value, getFloatVal("mass"), getFloatVal("x"), getFloatVal("y"), getFloatVal("velX"), getFloatVal("velY"))
+	 },
+	 buttonSet: function() {
+		 Universe.timeScale = getFloatVal("timeScale")
+		 Render.scale = getFloatVal("renderScale")
+		 Render.centerX = getFloatVal("centerX")
+		 Render.centerY = getFloatVal("centerY")
 	 }
 }
 
@@ -21,20 +38,45 @@ Universe.body = function (name, mass, x, y, velX, velY) {
 	this.velY = velY
 }
 
-/*Universe.body.prototype = {
-	tick: function () {
-		
+Universe.body.prototype = {
+	preTick: function () {
+		for (i in  Universe.bodyArray){
+			if (Universe.bodyArray[i] != this) {
+				if (Universe.bodyArray[i].x > this.x) {
+					var direction = Math.atan((Universe.bodyArray[i].y - this.y) / (Universe.bodyArray[i].x - this.x))  % (Math.PI * 2)
+				}
+				else {
+					var direction = Math.atan((Universe.bodyArray[i].y - this.y) / (Universe.bodyArray[i].x - this.x)) + Math.PI
+				}
+				var velD = Universe.bodyArray[i].mass / (Math.pow((Universe.bodyArray[i].x - this.x), 2) + Math.pow((Universe.bodyArray[i].y - this.y), 2))
+				this.velX += Math.cos(direction) *  velD / Universe.tickRate * Universe.timeScale
+				this.velY += Math.sin(direction) *  velD  / Universe.tickRate * Universe.timeScale
+			}
+		}
+	},
+	postTick: function () {
+		this.x += this.velX / Universe.tickRate * Universe.timeScale
+		this.y += this.velY / Universe.tickRate * Universe.timeScale
 	}
-}*/
+}
 
 var Render = {
 	canvas: document.getElementById("canvas"),
 	context: canvas.getContext("2d"),
+	scale: 1,
+	centerX: 0,
+	centerY: 0,
 	clear: function () {
 		Render.context.clearRect(0, 0, Render.canvas.width, Render.canvas.height)
 	},
 	renderObject: function (texture, x, y, size) {
 		var img = document.getElementById(texture)
-		Render.context.drawImage(img, x - (size / 2), y - (size / 2), size, size)
+		Render.context.drawImage(img, (x - (size / 2)) / Render.scale + 640 + Render.centerX, (y - (size / 2)) / Render.scale + 360 + Render.centerY, size / Render.scale, size / Render.scale)
 	}
 }
+
+function getFloatVal(id) {
+	return parseFloat(document.getElementById(id).value)
+}
+
+tickLoop = setInterval(Universe.tick, (1000 / Universe.tickRate))
